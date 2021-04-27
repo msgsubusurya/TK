b7fb9a1f58564deb6b7c1888b99fe21f729490cf
NoobMaster69 <71622790+NBM69@users.noreply.github.com>
2021-04-27 10:27:17 +0530

    Update ariatools.py

--- tortoolkit/functions/ariatools.py
+++ tortoolkit/functions/ariatools.py
@@ -81,14 +81,20 @@ def add_torrent(aria_instance, torrent_file_path):
 
 
 def add_url(aria_instance, text_url, c_file_name):
+    options = None
+    # if c_file_name is not None:
+    #     options = {
+    #         "dir": c_file_name
+    #     }
     uris = [text_url]
     # Add URL Into Queue
     try:
-        download = aria_instance.add_uris(
-            uris
-        )
+        download = aria_instance.add_uris(uris, options=options)
     except Exception as e:
-        return False, "**FAILED** \n" + str(e) + " \nPlease do not send SLOW links. Read /help"
+        return (
+            False,
+            "**FAILED** \n" + str(e) + " \nPlease do not send SLOW links. Read /help",
+        )
     else:
         return True, "" + download.gid + ""
 
